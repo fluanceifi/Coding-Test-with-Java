ba2197a6d3fc9117dac6ff8645e59064d85bf8ed
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2025-06-26 16:01:26 +0900

    Update 피보나치 수열 (메모이제이션).java

--- 동적 프로그래밍/피보나치 수열 (메모이제이션).java
+++ 동적 프로그래밍/피보나치 수열 (메모이제이션).java
@@ -18,8 +18,10 @@ class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        //100을 입력시 101은 있어야 출력가능
         int[] arr = new int[n+1];
 
+        //100번째를 출력하고 싶다면, 100-1을 해야 100번째가 출력됨 why? index는 0부터 시작
         System.out.println(fibonachi(arr, n-1));
         
 
