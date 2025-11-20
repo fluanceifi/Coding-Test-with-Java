023cc6db9ba442b680abcc2e7be0796b06dbda63
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2025-11-20 16:19:51 +0900

    Update 병사 배치하기.java

--- 유형별 기출문제/dp/병사 배치하기.java
+++ 유형별 기출문제/dp/병사 배치하기.java
@@ -1,37 +1,36 @@
 import java.util.*;
 
-public class Main{
-    public static int n;
-    public static int[] dp;
-    
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
+class Main {
+    public static int n, result;
+    public static int[] soldiers;
+    public static int[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
         n = sc.nextInt();
 
+        soldiers = new int[n];
         dp = new int[n];
-        
-        dp[0] = 1;
 
-        
-        int i2 = 0, i3 = 0, i5 = 0;
-
-        for(int i = 1 ; i < n ; i++){
-            dp[i] = Math.min(dp[i2] * 2, Math.min(dp[i3] * 3, dp[i5] * 5));
-
-            if(dp[i] == dp[i2] * 2){
-                i2++;
-            }
-            
-            if(dp[i] == dp[i3] * 3){
-                i3++;
-            }
-
-            if(dp[i] == dp[i5] * 5){
-                i5++;
-            }
+        for(int i = 0 ; i < n ; i++){
+            soldiers[i]=sc.nextInt();
+            dp[i]=1;
         }
 
-        System.out.println(dp[n-1]);        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                //만약 나(i)보다 전투력이 더 높은 상대(j)가 있다면, 상대방 뒤에 배치가 된다.
+                //그렇다면 내 기준으로 줄(dp[i])은 상대의 줄 길이(dp[j]) + 1 이 된다.
+                //하지만 만약 내 기존 줄보다 더 짧다면 포기할 것이다. (병사 수 가 최대 수가 되고 싶기 때문이다.)
+                if(soldiers[j] > soldiers[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            //최대값 갱ㅇ신
+            result = Math.max(result, dp[i]);
+        }
+
+        System.out.println(n - result);
     }
 }
